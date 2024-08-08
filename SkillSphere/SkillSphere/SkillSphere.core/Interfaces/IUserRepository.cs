cde90374d3696b0c3537559fa9562a876ca4acf1
Shreyash-Shedge <163419571+Shreyash-Shedge@users.noreply.github.com>
2024-08-08 15:21:51 +0530

    Delete SkillSphere/SkillSphere directory

--- SkillSphere/SkillSphere/SkillSphere.core/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Skillsphere.Core.Models;
-
-namespace SkillSphere.Core.Interfaces
-{
-    public interface IUserRepository
-    {
-        Task<User> GetUserByeEmailAsync(string email);
-    }
-}
--- SkillSphere/SkillSphere/SkillSphere.core/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Skillsphere.Core.Models
-{
-    public class Admin : User
-    {
-    }
-}
